1099a7753375758456efe717b5146d6ccef55542
Vitor Serrão <vitorwss@gmail.com>
2022-12-06 20:53:27 -0300

    remover

--- src/app/models/training.ts
+++ src/app/models/training.ts
@@ -1,14 +1,14 @@
-import { IModule } from 'src/app/models/modules'
+import { IModule } from './modules';
 
-export interface ITraining{
-    id:number;
-    url:string;
-    title:string;
-    description:string;
-    teacher:string;
-    duration:number;
-    active:boolean;
-    category:string;
-    date:Date;
-    modules:IModule[]
+export interface ITraining {
+  id: number;
+  url: string;
+  title: string;
+  description: string;
+  teacher: string;
+  duration: number;
+  active: boolean;
+  category: string;
+  date: Date;
+  modules: IModule[];
 }
--- src/app/modules/main/components/item-my-training/item-my-training.component.ts
+++ src/app/modules/main/components/item-my-training/item-my-training.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ITraining } from 'src/app/models/training';
+import { ITraining } from '../../../../models/training';
 import { AlertService } from '../../services/alert/alert.service';
 import { TrainingService } from '../../../../services/training/training.service';
+import { TrainingComponent } from '../../pages/training/training.component';
+import { IUser } from 'src/app/models/user';
+import { IRegistration } from 'src/app/models/registration';
 
 @Component({
   selector: 'pro-item-my-training',
@@ -10,7 +13,11 @@ import { TrainingService } from '../../../../services/training/training.service'
   styleUrls: ['./item-my-training.component.scss'],
 })
 export class ItemMyTrainingComponent implements OnInit {
-  userId!: number;
+  userId!: IUser;
+  UserActive!: IUser;
+  IdTraningRemove!: ITraining;
+  training: any;
+  IdTest!: number;
 
   @Input()
   item: ITraining = {
@@ -33,9 +40,31 @@ export class ItemMyTrainingComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {}
-
-  removeTraining() {
-    this.alertService.alertDeleteTraining();
+  getUserActive() {
+    this.trainingService
+      .getUserByToken(this.trainingService.token)
+      .subscribe((user: IUser) => {
+        return (this.UserActive = user);
+      });
+  }
+  getSqlTraining(idT: number, idU: IUser) {
+    this.trainingService.getTraining().subscribe((trainings) => {
+      this.training = trainings;
+      const result = this.training.filter((userT: any) => {
+        return userT.userId == idU.id && userT.trainingId == idT;
+      });
+      this.IdTest = result[0].id;
+      this.alertService.alertDeleteTraining(this.IdTest);
+    });
+  }
+  removeTraining(IdTraning: ITraining) {
+    this.IdTraningRemove = IdTraning;
+    this.trainingService
+      .getUserByToken(this.trainingService.token)
+      .subscribe((user: IUser) => {
+        this.UserActive = user;
+        this.getSqlTraining(this.IdTraningRemove.id, this.UserActive);
+      });
   }
 
   selectTraining(training: ITraining) {
@@ -48,7 +77,7 @@ export class ItemMyTrainingComponent implements OnInit {
     this.trainingService.PatchRecentTrainingsByUser(id, dateRefresh);
   }
 
-  formatarDuracao(duracao:number):any{
-    return duracao.toString().replace(':00:00', '')
+  formatarDuracao(duracao: number): any {
+    return duracao.toString().replace(':00:00', '');
   }
 }
